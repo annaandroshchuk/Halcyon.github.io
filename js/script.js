1eaec7cc2dcc1b440e07d2327d7cf797427d22f7
annaandroshchuk <annaandroshchuk91@gmail.com>
2025-05-08 20:46:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:35,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
